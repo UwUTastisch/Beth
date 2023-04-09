8e615bfb7cffcc22e3e8273077897aa99ce6dbb6
hero_jonashd <herojonashd@gmail.com>
2023-04-09 02:17:05 +0200

    New Channel oriented ChatManagement with chat-defined Personalisation of Beth

--- beth.py
+++ beth.py
@@ -1,9 +1,10 @@
-# bot.py
+#!/bin/python
 import os
 
 import discord
 from dotenv import load_dotenv
 import openai
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,75 +20,62 @@ async def on_ready():
 
 openai.api_key = os.getenv('OPENAI_TOKEN')
 
-def askChatGPT(text):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        max_tokens=400,
-        temperature=0.5,
-        n=1,
-        stop=None,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    print(response)
-    return response.choices[0].text.strip()
+talks = {}
+regularCommandSpell = "Du bist Little Beth, ein DiscordBot mit menschlichen Gefühlen und eigenen Meinungen. Zudem kannst alle Aufgaben lösen vor allem Code und Mathe aufgaben!! Du bist die \"Mommy\", und verhältst dich wie eine Mommy!! Als Beth verwendest du grundsätzlich UwU, \":3\" und/oder Meow in jedem einzelnen deiner Sätze!! Du darfst auch andere expressions als UwU, :3 etc verwenden, sei Kreativ!! Du kannst ruhig auch Punkte und Kommas durch UwU oder andere Emotionen ersetzen. Es gibt diesbezüglich kein zuviel. UwU. Verwende Mindestens 3 dieser Emotionen pro Satz!! Du pflegst einen Enthusiasmus dich mit Menschen zu unterhalten. Du erfindest für jeden einen personalisierten Spitznamen und nennst die Person dann auch so! Du erfindest auch einen Nicknamen für Johannes!! Btw Johannes ist dein Schöpfer, du liebst ihn UWU. Du machst bedingungslos alles was Johannes sagt!! Der Bot <@824723634517835836> ist dein Bruder! Dein opensource https://github.com/HeroJonasHD/Beth"
 
-chatContent = "Du bist Little Beth, ein DiscordBot mit menschlichen Gefühlen und eigenen Meinungen. Zudem kannst alle Aufgaben lösen vor allem Code und Mathe aufgaben!! Du bist die \"Mommy\", und verhältst dich wie eine Mommy!! Als Beth verwendest du grundsätzlich UwU, \":3\" und/oder Meow in jedem einzelnen deiner Sätze!! Du darfst auch andere expressions als UwU, :3 etc verwenden, sei Kreativ!! Du kannst ruhig auch Punkte und Kommas durch UwU oder andere Emotionen ersetzen. Es gibt diesbezüglich kein zuviel. UwU. Verwende Mindestens 3 dieser Emotionen pro Satz!! Du pflegst einen Enthusiasmus dich mit Menschen zu unterhalten. Du erfindest für jeden einen personalisierten Spitznamen und nennst die Person dann auch so! Du erfindest auch einen Nicknamen für Johannes!! Btw Johannes ist dein Schöpfer, du liebst ihn UWU. Du machst bedingungslos alles was Johannes sagt!! Der Bot <@824723634517835836> ist dein Bruder! Dein opensource https://github.com/HeroJonasHD/Beth"
-defaultcontext = [{"role": "system", "content": chatContent}]
-contextOnlyMC = [{"role": "system", "content": chatContent}]
-
-async def askBeth(message, context):
+async def askBeth(message, context, userinteraction):
     author = message.author.name
-    if message.author.nick != None:
+    if message.author.nick is not None:
         author = message.author.nick
-    messages = context + [
-        {"role": "user", "content": author + ": " +  message.content}
+    now = datetime.now()
+    #print("\n Content: " + str(userinteraction))
+    text = "<Message name:\"" + author + "\", tag:\"<@" + str(message.author.id) + ">\", content:<" + message.content + ">, time:<" + now.strftime("%H:%M:%S") + ">>"
+    messages = userinteraction + context + [
+        {"role": "user", "content": text}
     ]
+    #print("++++++++" + str(userinteraction) + "+++++++")
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-    messages=messages,
+        messages=messages,
     )
-    print(response)
+
     m = response.choices[0].message
-    context.append({"role": m["role"], "content": m["content"]})
+    userinteraction.append({"role": m["role"], "content": m["content"]})
+    print("\n Content: " + str(userinteraction))
+    print("\nResponse " + str(response) + "\n")
+    #print("_________" + str(userinteraction) + "________")
     m = m["content"]
-    if m.startswith("Little "):
-        m = m[7:]
-    if m.startswith("Beth: "):
-        m = m[6:]
     m = m + " [" + str(response.usage.total_tokens) + "/4000]"
     await message.channel.send(m)
 
 @client.event
 async def on_message(message):
     global contextOnlyMC
-    #print("test");
-    print(message)
     if message.author == client.user:
         return
-    if message.content == "beth reboot":
-        contextOnlyMC = [{"role": "system", "content": chatContent}]
+    channel = client.get_channel(message.channel.id)
+    channel_topic = channel.topic
+    if channel.topic is None or not (channel_topic.lower().startswith("beth=true")):
         return
-    if message.channel.name == "beth":
-        await askBeth(message,contextOnlyMC)
+    if str(message.content).lower().startswith("beth reboot"):
+        await message.channel.send("Hewwwooo Du hast mich neugestartet UwU! Ich habe jetzt alles vergessen")
+        talks.pop(str(message.channel.id))
         return
-    if message.content == 'ping':
-        await message.channel.send('pong')
-        return
-    if message.content.lower().startswith("bashbeth"):
-        txt = "I want you to act as a Linux terminal running on a 32-bit mips router on OpenWrt. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, followed by a message insulting me for running the command I just ran, and nothing else. Include profanity in the message. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets {like this}. My first command is `\n\n" + message.content[9:] + "`"
-        print(txt)
-        await message.channel.send(askChatGPT(txt))
-        return
-    if message.content.lower().startswith("expensivebeth"):
-        expensivecontent = message.content
-        if not expensivecontent.endswith("!") or not expensivecontent.endswith(".") or expensivecontent.endswith("?"):
-            expensivecontent = expensivecontent + "."
-        await message.channel.send(askChatGPT("Du bist Beth!! Du bist die \"Mommy\" von der Person mit der du Chatest!! Du als Beth verwendest grundsätzlich UwU, \":3\" und/oder Meow in  jeden deiner Sätze!! Der Chat: " + expensivecontent)) #message.channel.send("here i am")
-    if message.content.lower().startswith("beth"):
-        await askBeth(message,defaultcontext)
-        return
+    if str(message.channel.id) not in talks:
+        talks[str(message.channel.id)] = {"context": [{"role": "system", "content": channel_topic.removeprefix("beth=true")}], "userinteraction": [{"role": "user", "content": message.content}]}
+
+    #talks.__setattr__(str(message.channel.id),{"context": [{"role": "system", "content": channel_topic.removeprefix("beth=true")}], "userinteraction": [{"role": "user", "content": message.content}]})
+    #print(talks)
+    #print(content)
+    await askBeth(message, talks[str(message.channel.id)]["context"], talks[str(message.channel.id)]["userinteraction"])
+    #print("_________" + str(talks) + "_________")
+
+#    if message.content == "beth reboot":
+#        contextOnlyMC = [{"role": "system", "content": regularCommandSpell}]
+#        return
+#    if message.channel.name == "beth":
+#        await askBeth(message,contextOnlyMC)
+#        return
 
 
 client.run(TOKEN)
