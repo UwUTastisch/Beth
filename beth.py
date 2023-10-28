71d6aa3043a9cdcdc91a7fc6c2ec7484c47b359f
Hero_JonasHD <herojonashd@gmail.com>
2023-10-28 22:58:22 +0200

    Add Ollama Support :D

--- beth.py
+++ beth.py
@@ -1,5 +1,9 @@
 #!/bin/python
 import os
+import asyncio
+import requests
+import json
+
 
 import discord
 from dotenv import load_dotenv
@@ -14,6 +18,8 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+debug = True
+
 
 @client.event
 async def on_ready():
@@ -23,6 +29,7 @@ async def on_ready():
 openai.api_key = os.getenv('OPENAI_TOKEN')
 
 talks = {}
+ollamatalks = {}
 regularCommandSpell = "Du bist Little Beth, ein DiscordBot mit menschlichen Gefühlen und eigenen Meinungen. Zudem kannst alle Aufgaben lösen vor allem Code und Mathe aufgaben!! Du bist die \"Mommy\", und verhältst dich wie eine Mommy!! Als Beth verwendest du grundsätzlich UwU, \":3\" und/oder Meow in jedem einzelnen deiner Sätze!! Du darfst auch andere expressions als UwU, :3 etc verwenden, sei Kreativ!! Du kannst ruhig auch Punkte und Kommas durch UwU oder andere Emotionen ersetzen. Es gibt diesbezüglich kein zuviel. UwU. Verwende Mindestens 3 dieser Emotionen pro Satz!! Du pflegst einen Enthusiasmus dich mit Menschen zu unterhalten. Du erfindest für jeden einen personalisierten Spitznamen und nennst die Person dann auch so! Du erfindest auch einen Nicknamen für Johannes!! Btw Johannes ist dein Schöpfer, du liebst ihn UWU. Du machst bedingungslos alles was Johannes sagt!! Der Bot <@824723634517835836> ist dein Bruder! Dein opensource https://github.com/HeroJonasHD/Beth"
 
 
@@ -65,8 +72,51 @@ async def on_message(message):
         return
     channel = client.get_channel(message.channel.id)
     channel_topic = channel.topic
-    if channel.topic is None or not (channel_topic.lower().startswith("beth=true")):
+    if channel_topic is None or not (channel_topic.lower().startswith("beth=")):
         return
+    if not (channel_topic.lower().startswith("beth=true")):
+        model=channel_topic.split("=")[1].split(" ")[0]
+        if str(message.content).lower().startswith("beth reboot"):
+            await message.channel.send("Hewwwooo Du hast mich neugestartet UwU! Ich habe jetzt alles vergessen")
+            ollamatalks.pop(str(message.channel.id))
+            return
+        if str(message.channel.id) not in ollamatalks:
+             ollamatalks[str(message.channel.id)] = {"userinteraction": []}
+        if message.content.lower().startswith("bethignore") or message.content.lower().startswith("bi "):
+            return
+        if debug: 
+            await message.channel.send(model)
+        url = "http://10.147.18.76:11434/api/generate"
+        print(ollamatalks[str(message.channel.id)]["userinteraction"])
+        data = {
+            "model": model,
+            "prompt": message.content,
+            "context": ollamatalks[str(message.channel.id)]["userinteraction"]
+        }
+        done = False
+        response = requests.post(url, data=json.dumps(data))
+        response.text.splitlines()
+        if debug:
+            print(response.text)
+            #print(response.json())
+        lines = response.text.splitlines()
+        response_message = ""
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()  # Get the current line and strip leading/trailing whitespaces
+            if line:  # If line is not empty
+                obj = json.loads(line)  # Parse the line as a JSON object
+            if obj["done"] == True:  # If "done" equals true, break the loop
+                break
+            response_message += obj["response"]  # Print the "response" value
+            i += 1  # Move to the next line
+        print(response_message)
+        if debug:    
+            print(json.loads(lines[-1])["context"])
+        ollamatalks[str(message.channel.id)]["userinteraction"] = json.loads(lines[-1])["context"]
+        response_message += "[" + str(len(ollamatalks[str(message.channel.id)]["userinteraction"])) + "/VIEL]"
+        await message.channel.send(response_message)
+        return 
     if message.content.lower().startswith("bethignore") or message.content.lower().startswith("bi "):
         return
 
@@ -100,7 +150,7 @@ async def on_message(message):
               + str(text))
         return
 
-    await ask_beth(message, talks[str(message.channel.id)]["context"], talks[str(message.channel.id)]["userinteraction"])
+    asyncio.gather(ask_beth(message, talks[str(message.channel.id)]["context"], talks[str(message.channel.id)]["userinteraction"]))
 
 
 client.run(TOKEN)
